fc81b4e020edb42af65a1f7c95d2a51064a75286
Pranav Balaji <balaji.pranav@outlook.in>
2020-04-23 21:50:06 +0530

    yg

--- master/v2.1_sample.py
+++ master/v2.1_sample.py
@@ -1,19 +1,18 @@
 # New gui login script instead of built-in eww..
 # Full changelog on github
-# Code-copiers will be punished. Def. Period. Gimme more words.
-# S
+# Uses pysimplegui
+# Sample version
+
 import getpass, time, pathlib, os, Path
-import os #library used to open the notepad application to display the sales records
+import os #used to execute Windows-level commands
 if os.path.exists(r'userblock.zconf'):
-    print("Decrypting authenication blobs...")
-    print(" ")
     p = Path('userblock.zconf')
     p.rename(p.with_suffix('.txt'))
 if os.path.exists(r'userblock.txt'):
-    userblock = open(r"userblock.txt","r") #Opening / creating (if it doesn't exist already) the .txt record file
-    valfn = 0
+    userblock = open(r"userblock.txt","r") #Opens/ creates the registry file
+    valfn = 1 #all good
 else:
-    valfn = 0
+    valfn = 0 #login bypass
 if os.path.exists(r'userblock.txt'):
     userblock.close()  
     os.remove(r'userblock.txt')
@@ -25,23 +24,21 @@ elif os.path.exists(r'userblock.zconf'):
 data = {"1":40000, "2":55000, "3":67000, "4":25000, "5":21000, "6":14000, "7":13000, "8":220000, "9":4500, "10":17000, "11":1200, "12":3700, "13":4500, "14":2200, "15":700, "16":2750, "17":6499, "18":1499, "19":799, "20":27000, "21":6750, "22":2100, "23":1199, "24":3210, "25":989, "26":750, "27":1700, "28":600, "29":2175, "30":890, "31":2100, "32":7158, "33":597, "34":347, "35":500, "36":300, "37":1097, "38":80000, "39":87900, "40":23790}
 namie = {"1":"TV 4K OLED 50", "2":"TV FHD OLED 50", "3":"8K QLED 80", "4":"Redmi K20 PRO", "5":"Redmi K20", "6":"Redmi Note 8 PRO", "7":"POCOPHONE F1", "8":"Mi MIX ALPHA", "9":"Wireless Headphones", "10":"Noise-Cancelling Wireless Headphones", "11":"Essentials Headphones", "12":"Gaming Headphones", "13":"Truly-Wireless Eadphones", "14":"Neckband-Style Wireless Earphones", "15":"Essentials Earphones", "16":"Gaming Earphones", "17":"30W Bluetooth Speakers", "18":"10W Bluetooth Speakers", "19":"Essentials Bluetooth Speaker", "20":"ULTRA Home Theatre", "21":"Essentials Home Theatre", "22":"  Wired Speaker - 5.1", "23":"  Essentials Wired Speaker - STEREO", "24":"Tactical Power Bank 30000mah", "25":"Essentials Power Bank 10000mah", "26":"Essentials Mouse", "27":"Logitech RGB Gaming Mouse with Traction & Weight Adjustment", "28":"Tactical Essentials Keyboard", "29":"Mechanical Cherry MX (Red) RGB Gaming Keyboard", "30":"Polowski Tactical Flashlight", "31":"OneFiber Wi-Fi Router AX17", "32":"Mijia Mesh Wi-Fi Router", "33":"lapcare 120W Laptop Adapter", "34":"lapcare 60W Laptop Adapter", "35":"Spigen Phone Case(s)", "36":"Essentials Phone Charger 10W", "37":"HyperPower Type-C Gallium-Nitride Charger 100W", "38":"ASUS Zephyrus G14 Gaming Laptop", "39":"L XPS 15 Content Creator's Laptop", "40":"Hewlett-Packard Essential's Student's Laptop (Chromebook)"}
 
+'''
 def floodscreen():
     import cv2 
     image = cv2.imread("imagepx.png")
-    cv2.imshow("Initializing... ", image)
+    cv2.imshow("Please wait...", image)
     cv2.waitKey(3000)
     cv2.destroyAllWindows()
+'''
 
-
-print(" Licensed under the GNU PUBLIC LICENSE")
-print("<DBFA>  Copyright (C) 2020 Pranav Balaji")
-print(" ")
-print("Visit: www.github.com/deltaonealpha/deltaBillingFramework for complete licensing terms. ")
+print("Welcome to the new generalized billing framework!")
 time.sleep(1.3)
 command = "cls"
-os.system(command)
+os.system(command) #sys command cls
  
-def mainmenu(): #defining a function for the main menu
+def mainmenu():
     from colorama import init, Fore, Back, Style #color-settings for the partner/sponsor adverts
     init(convert = True)
     print(Fore.RED) #red-line to indicate program start
@@ -63,18 +60,16 @@ def mainmenu(): #defining a function for the main menu
     print()
  
 
-#void-setup phase
-from datetime import datetime  #for reporting the billing time and date
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")  #datetime object containing current date and time
-logger = open(r"registry.txt", "a+")  #Opening / creating (if it doesn't exist already) the .txt record file
+#void-setup
+from datetime import datetime  #reports billing time-date
+now = datetime.now() #current acc to sys time
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+logger = open(r"registry.txt", "a+")  #opens/creates the registry file
 logger.write("----------------------------------------- \n")
-logger.write("DBFA Billing Framework by Pranav Balaji\n")
-logger.write("Licensed under the GNU PUBLIC LICENSE\n")
-logger.write('ed')
+logger.write('User identified from login: ed')
 logger.write("\n")
 logger.write("Automated Store Registry:\n")
-import mysql.connector  #to connect to the SQL database (local)
+import mysql.connector  #sql connector
 import time  #to provide delays to make the system run seamlessly
 conn = mysql.connector.connect(host='localhost', database='delta', user='root', password='shieldlogmein')  #sql connection parameters
 cursor = conn.cursor()
@@ -99,11 +94,10 @@ win32api.MessageBox(0, 'Please review licensing terms before usage', 'Licensing!
 print("Heyy there!",  'ed')
 time.sleep(1.34)
 if valfn == 1:
-    logger.write("Oauth bypass - registering for security")
+    logger.write("Oauth bypass - registering this for security logs")
     time.sleep(1)
     print("-------DBFA standardised billing framework-------")
-    time.sleep(0.5)
-    print("Security is something to be valued primemostly, in today's digital age.")
+    print("We see you there, hackerman.")
     time.sleep(1)
     print("It has been detected that you have bypassed the login process.")
     time.sleep(1)
@@ -181,12 +175,12 @@ while(1): #while (always) true
         cursor.execute("select * from cust")
         row = cursor.fetchone()
         #takes values from the SQL database
-        countguy = 0
+        countss = 0
         while row is not None:
             displayguyfornouse = row
             row = cursor.fetchone()
-            countguy+=1
-        incfac = countguy + 1
+            countss+=1
+        incfac = countss + 1
         print("Registering customer with ID: ", incfac)
         custname = input("Name: ")
         email = input("Customer's E-mail ID: ")
